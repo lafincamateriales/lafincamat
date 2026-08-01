4a91f2bbfad505c3059ade0b20abfc79c58961be
lafincamateriales <lafincamaterialesnavojoa@gmail.com>
2026-08-01 14:01:51 -0700

    Add product list to productos.js

---
+++ productos.js
@@ -0,0 +1,93 @@
+const productos = [
+
+{
+    id:1,
+    nombre:"Cal Hidratada",
+    categoria:"Cal",
+    precio:130,
+    unidad:"Saco",
+    imagen:"img/productos/cal.png",
+    descripcion:"Cal Hidratada Calidra."
+},
+
+{
+    id:2,
+    nombre:"Cemento Moctezuma",
+    categoria:"Cemento",
+    precio:165,
+    unidad:"Saco",
+    imagen:"img/productos/cemento.png",
+    descripcion:"Cemento Portland."
+},
+
+{
+    id:3,
+    nombre:"Mortero Moctezuma",
+    categoria:"Mortero",
+    precio:147,
+    unidad:"Saco",
+    imagen:"img/productos/mortero.png",
+    descripcion:"Mortero listo para construcción."
+},
+
+{
+    id:4,
+    nombre:"Yeso YINSA",
+    categoria:"Yeso",
+    precio:138,
+    unidad:"Saco",
+    imagen:"img/productos/yeso.png",
+    descripcion:"Yeso para construcción."
+},
+
+{
+    id:5,
+    nombre:"Clavo 2 1/2",
+    categoria:"Clavos",
+    precio:43,
+    unidad:"Kg",
+    imagen:"img/productos/clavo.png",
+    descripcion:"Clavo de acero."
+},
+
+{
+    id:6,
+    nombre:"Alambre Recocido",
+    categoria:"Alambre",
+    precio:37,
+    unidad:"Kg",
+    imagen:"img/productos/alambre.png",
+    descripcion:"Alambre recocido."
+},
+
+{
+    id:7,
+    nombre:"Armex 15-20",
+    categoria:"Armex",
+    precio:167,
+    unidad:"Pieza",
+    imagen:"img/productos/armex1520.png",
+    descripcion:"Armex prefabricado."
+},
+
+{
+    id:8,
+    nombre:"Armex 15-15",
+    categoria:"Armex",
+    precio:167,
+    unidad:"Pieza",
+    imagen:"img/productos/armex1515.png",
+    descripcion:"Armex prefabricado."
+},
+
+{
+    id:9,
+    nombre:"Varilla 3/8",
+    categoria:"Varilla",
+    precio:158,
+    unidad:"Pieza",
+    imagen:"img/productos/varilla38.png",
+    descripcion:"Varilla corrugada."
+}
+
+];
